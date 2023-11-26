f848b51b45e6989abdf6784a8addf6eef7a20b76
kimze <crk33333@gmail.com>
2023-11-26 11:43:17 +0900

    Spring : BindingResult 필드에러: th:errors/th:errorclass 글로벌에러 : #fields.hasGlobalError

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -7,6 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -42,33 +45,33 @@ public class ValidationItemControllerV2 {
         return "validation/v2/addForm";
     }
 
-    @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
+    @PostMapping("/add")                        //BindingResult는 반드시 메시지를 바인딩 할 객체 뒤에 선언되어야 함
+    public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
-        //ㄱㅓㅁ증 오류 결과를 보관
-        Map<String, String> errors = new HashMap<>();
+        //필드 단위의 오류는 FieldError
         if(!StringUtils.hasText(item.getItemName())){
-            errors.put("itemName", "상품 이름은 필수입니다.");
+             bindingResult.addError(new FieldError("item","itemName","상품 이름은 필수 입니다."));
         }
         if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000){
-            errors.put("price","가격은 1,000 ~ 1,000,000 까지 허용합니다.");
+            bindingResult.addError(new FieldError("item","price","가격은 1,000 ~ 1,000,000 까지 허용합니다."));
         }
         if(item.getQuantity() == null || item.getQuantity() >= 9999 ){
-            errors.put("quantity", "수량은 최대 9,999 까지 허용합니다.");
+            bindingResult.addError(new FieldError("item","quantity","수량은 최대 9,999 까지 허용합니다."));
         }
 
-        //특전 필드가 아닌 복합 룰 검증
+        //특정 필드가 아닌 복합 룰 검증
         if(item.getPrice() != null && item.getQuantity() != null){
             int resultPrice = item.getPrice() * item.getQuantity();
-            if(resultPrice < 10000) {
-                errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice);
+            if(resultPrice < 10000) { //특정 필드의 오류가 아닌 오브젝ㅌ 자체의 오류는 ObjectError
+                bindingResult.addError(new ObjectError("item","가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
             }
         }
 
         //검증에 실패하면 다시 입력 폼으로
-        if(!errors.isEmpty()) {
-            log.info("errors={}"+errors);
-            model.addAttribute("errors", errors);
+        if(bindingResult.hasErrors()) {
+            log.info("errors={}"+bindingResult);
+            // BindingResult는 모델에 담지 않아도 뷰로 넘어감 . 스프링의 오류메시지 매커니즘이므로..
+            //model.addAttribute("errors", errors);
             return "validation/v2/addForm";
         }
 
